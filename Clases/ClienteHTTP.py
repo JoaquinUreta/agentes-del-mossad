4d0c256a378a7106997918724263cdec6889e4cd
Pablo Gutiérrez <valhirwrld@gmail.com>
2026-05-04 02:10:26 -0400

    añadido timeout de 10 seg

--- Clases/ClienteHTTP.py
+++ Clases/ClienteHTTP.py
@@ -1,7 +1,7 @@
 import http.client
 
 class ClienteHTTP:
-    def buscarurl(self, url):
+    def buscarurl(self, url, timeout=10): 
         if "/" in url:
             host, path = url.split("/", 1)
             path = "/" + path
@@ -10,13 +10,16 @@ class ClienteHTTP:
             path = "/"
 
         try:
-            conn = http.client.HTTPConnection(host, 80)
-            #conn = http.client.HTTPSConnection(host, 443) funciona
+            conn = http.client.HTTPSConnection(host, 443, timeout=timeout)
             conn.request("GET", path)
             response = conn.getresponse()
             status = response.status
             html = response.read().decode("utf-8", errors="replace")
             conn.close()
             return html, status
+
+        except TimeoutError:
+            return "<h1>Tiempo de espera agotado</h1>", None
+
         except Exception as e:
             return f"<h1>Error al conectar</h1><p>{e}</p>", None
