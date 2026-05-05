5c49d789f0b634196992ae4ac6d41a92b1122613
JoaquinUreta <jureta23@alumnos.utalca.cl>
2026-05-05 17:12:15 -0400

    funcion_enviar_gets

--- Clases/ClienteHTTP.py
+++ Clases/ClienteHTTP.py
@@ -1,6 +1,20 @@
 import http.client
 
 class ClienteHTTP:
+
+    def enviar_get(self, host, ruta):
+        conexion = http.client.HTTPConnection(host,80) #El host en teoria es el que nos dio el profe y el 80 se refiere al puerto
+
+        headers = {
+            "Host": host,
+            "User-Agent": "ClienteHTTP/1.0"
+        }
+        
+        #Esta es la solicitud como tal que enviara el codigo hacia el servidor.
+        conexion.request("GET", ruta, headers=headers) 
+
+        return conexion
+    
     def buscarurl(self, url, timeout=10): 
         if "/" in url:
             host, path = url.split("/", 1)
